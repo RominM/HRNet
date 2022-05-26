3d0bc7d9e71bdf5a55b571384d7c1f2cef708525
RominM <rominmanogil.off@gmail.com>
2022-05-26 21:32:49 +0200

    feat: :alembic: useState in App
    to pass the data from create Employee page to table in Employee list page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Footer from './components/Layout/Footer';
 import Header from './components/Layout/Header';
@@ -5,12 +6,19 @@ import CreateEmployee from './pages/CreateEmployee';
 import EmployeeList from './pages/EmployeeList';
 
 function App() {
+   const [records, setRecords] = useState([]);
    return (
       <div className="bloc-page">
          <Header />
          <Routes>
-            <Route path="/" element={<CreateEmployee />} />
-            <Route path="/employee-list" element={<EmployeeList />} />
+            <Route
+               path="/"
+               element={<CreateEmployee setRecords={setRecords} />}
+            />
+            <Route
+               path="/employee-list"
+               element={<EmployeeList records={records} />}
+            />
          </Routes>
          <Footer />
       </div>
