73beea675ff6794ee21414eeceff0595a6a298c2
RominM <rominmanogil.off@gmail.com>
2022-05-27 15:03:05 +0200

    range css files

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 // import reportWebVitals from './utils/perf/reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-import './styles/reset.css';
-import './styles/app.css';
 import './styles/sass/main.scss';
 
 ReactDOM.render(
