ac03046ec9fc99c307ab38163bfc65cc67faa162
RominM <rominmanogil.off@gmail.com>
2022-06-03 11:55:32 +0200

    perf: :zap: work on html & css
    give a size to img then attr to css & a font-display: swap

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import copyright from './../../assets/images/copyright-symbol-svgrepo-com.svg';
 
 const Footer = () => {
-   return (
-      <footer>
-         <div className="copyright">
-            <img src={copyright} alt="copyright logo" />
-            <span>2022 HRNet | All Rigth Reserved</span>
-         </div>
-      </footer>
-   );
+  return (
+    <footer>
+      <div className="copyright">
+        <img src={copyright} alt="copyright logo" width="17" height="17" />
+        <span>2022 HRNet | All Rigth Reserved</span>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
